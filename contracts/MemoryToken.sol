6a5c3182a9985c0e412a5422289b1b382b2110b9
Marlon Ramos <marlon_raoni10@hotmail.com>
2022-01-05 00:20:42 -0300

    Creating mint method

--- contracts/MemoryToken.sol
+++ contracts/MemoryToken.sol
@@ -13,7 +13,15 @@ contract MemoryToken is ERC721URIStorage {
 
     }
 
-    function mint(address wallet, string memory _tokenURI) public returns(uint256) {
+    function mint(address _wallet, string memory _tokenURI) public returns(uint256) {
 
+        uint256 tokenId = _tokenIdCounter.current();
+
+        _safeMint(_wallet, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        _tokenIdCounter.increment();
+
+        return tokenId;
     }
 }
