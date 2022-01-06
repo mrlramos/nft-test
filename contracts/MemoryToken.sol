33ea37f8b4bf80f8f97126521ac9b0f1b0aa01dd
Marlon Ramos <marlon_raoni10@hotmail.com>
2022-01-06 02:34:32 -0300

    Creating methods on MemoryToken

--- contracts/MemoryToken.sol
+++ contracts/MemoryToken.sol
@@ -5,26 +5,59 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MemoryToken is ERC721URIStorage {
+contract MemoryToken is ERC721Enumerable, ERC721URIStorage {
 
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Memory Token", "MMT") {
-
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }    
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }    
+
+    constructor() ERC721("Memory Token", "MMT")  {}
 
     function mint(address _wallet, string memory _tokenURI) public returns(uint256) {
 
         uint256 tokenId = _tokenIdCounter.current();
 
         _safeMint(_wallet, tokenId);
-        _setTokenURI(tokenId, _tokenURI);
+        _setTokenURI(tokenId, _tokenURI); 
 
         _tokenIdCounter.increment();
 
         return tokenId;
     }
+
 }
